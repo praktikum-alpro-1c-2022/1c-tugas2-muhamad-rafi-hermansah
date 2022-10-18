dafb8b8415b5c668e196024b509aac35874e81c5
Muhamad Rafi Hermansah <114910465+MRafiHermasah@users.noreply.github.com>
2022-10-18 08:30:24 +0700

    Add files via upload

---
+++ rumus persegi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	double s, luas;
+	
+	cout << "masukkan s : ";
+	cin >> s;
+		
+	luas = s*s;
+	
+	cout << "luas nya adalah : " << luas << endl;
+	
+	return 0;
+}
